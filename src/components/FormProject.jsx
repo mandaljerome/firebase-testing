26077c438ba1e99fec758e1d535e5b76dc167370
Jerome Mandal <mandaljerome@yahoo.com>
2023-07-03 22:38:10 +0800

    2023/07/03

--- src/components/FormProject.jsx
+++ src/components/FormProject.jsx
@@ -2,21 +2,42 @@ import React, { useState } from 'react'
 import './FormProject.scss'
 
 const FormProject = () => {
-   const [project, setproject] = useState({ data: 'try' })
+   const [project, setProject] = useState({
+      title: '',
+      description: '',
+      date: '',
+   })
+
+   const eventChangeHandler = (e) => {
+      setProject(() => {
+         return { ...project, [e.target.id]: e.target.value }
+      })
+      console.log(e.target)
+   }
 
    const submitHandler = async (e) => {
       e.preventDefault()
 
-      const response = await fetch(
-         'https://sample-project-3f4b9-default-rtdb.asia-southeast1.firebasedatabase.app/projects.json',
-         {
-            method: 'POST',
-            body: JSON.stringify(project),
-            headers: {
-               'Content-Type': 'application/json',
-            },
-         }
-      )
+      try {
+         const response = await fetch(
+            'https://sample-project-3f4b9-default-rtdb.asia-southeast1.firebasedatabase.app/projects.json',
+            {
+               method: 'POST',
+               body: JSON.stringify(project),
+               headers: {
+                  'Content-Type': 'application/json',
+               },
+            }
+         )
+
+         setProject({
+            title: '',
+            description: '',
+            date: '',
+         })
+      } catch (err) {
+         throw err
+      }
    }
 
    return (
@@ -25,22 +46,36 @@ const FormProject = () => {
             <form onSubmit={submitHandler}>
                <div className='form-control'>
                   <label htmlFor='project-title'>Title</label>
-                  <input type='text' name='title' id='project-title' />
+                  <input
+                     type='text'
+                     name='title'
+                     id='title'
+                     value={project.title}
+                     onChange={eventChangeHandler}
+                  />
                </div>
 
                <div className='form-control'>
                   <label htmlFor='project-description'>Description</label>
                   <textarea
                      name='description'
-                     id='project-description'
+                     id='description'
                      cols='30'
                      rows='10'
+                     value={project.description}
+                     onChange={eventChangeHandler}
                   ></textarea>
                </div>
 
                <div className='form-control'>
                   <label htmlFor='date-created'>Date</label>
-                  <input type='text' name='date' id='date-created' />
+                  <input
+                     type='date'
+                     name='date'
+                     id='date'
+                     value={project.date}
+                     onChange={eventChangeHandler}
+                  />
                </div>
 
                <button>Submit</button>
